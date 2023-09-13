0ee4925ea8c8c21db523efa23b34332ade3a928a
Leonardo <leoccapra@gmail.com>
2023-09-13 11:40:12 -0300

    Create index.py

---
+++ construção de algoritmo/ex001 - Un002/index.py
@@ -0,0 +1,9 @@
+nome = input('Digite seu nome: ')
+
+idade = input('Quantos anos voce tem? :')
+
+print('Olá',nome, '!')
+print('Que legal, você tem', idade, 'anos !')
+universidade = input('Onde você estuda? : ')
+
+print('A', universidade, 'é uma boa universidade! Parabéns!')
