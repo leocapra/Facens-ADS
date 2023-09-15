9c2d9e566875359b10d3b726149bbb078999ddda
Leonardo <leoccapra@gmail.com>
2023-09-15 11:13:41 -0300

    Update main.py

---
+++ construção de algoritmo/ex002 - Un003/main.py
@@ -0,0 +1,11 @@
+
+print("Coloque apenas numero inteiros!")
+notaA = int(input("Digite sua primeira nota:"))
+
+notaB = int(input("Digite sua segunda nota:"))
+
+media = (notaA + notaB) / 2
+
+aprovado = media >= 6
+
+print("Aluno aprovado:", aprovado)
