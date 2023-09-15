1749a983f9a2b1fb0651d769133c14f29157eb72
Leonardo <leoccapra@gmail.com>
2023-09-15 12:08:55 -0300

    Update main.py

--- construção de algoritmo/ex002 - Un003/main.py
+++ construção de algoritmo/ex002 - Un003/main.py
@@ -6,6 +6,8 @@ notaB = float(input("Digite sua segunda nota:"))
 
 media = (notaA + notaB) / 2
 
-aprovado = media >= 6
+aprovado = media >= 6 and notaA > 8
 
 print("Aluno aprovado:", aprovado)
+
+#Exercicio de operadores relacionais e operadores lógicos!
